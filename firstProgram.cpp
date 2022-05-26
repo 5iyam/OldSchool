13e8a11d08427357458f1fc07368d7f9dead395f
5iyam <siyamnurrahman@gmail.com>
2022-05-26 20:14:53 +0600

    first-program

---
+++ firstProgram.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+    cout<<"        *"<<endl<<"       ***"<<endl<<"      *****"<<endl<<"     *******"<<endl<<"    *********"<<endl<<"   ***********"<<endl<<"  *************"<<endl<<" **************";
+    return 0;
+}
+
+/*
+
+  *
+ ***
+*****
+
+*/
